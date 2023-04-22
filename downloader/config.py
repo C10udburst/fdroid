bc6581cee49a06b487a727e2c7ec6de9061c895c
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-04-22 09:36:02 +0200

    add discord

--- downloader/config.py
+++ downloader/config.py
@@ -1,4 +1,5 @@
 import modules
+from httpx import AsyncClient
 
 class YoutubeRevancedExtended(modules.GithubReleases):
     def __init__(self):
@@ -7,16 +8,29 @@ class YoutubeRevancedExtended(modules.GithubReleases):
     def filter_asset(self, asset) -> bool:
         return asset['name'].startswith('youtube-revanced-extended')
 
-class XManager(modules.Telegram):
+class Spotify(modules.Telegram):
     def __init__(self):
         super().__init__("xManagerSupport")
 
     def filter_media(self, media) -> bool:
         name = media['document']['attributes'][0]['file_name']
-        return name.startswith("Spotify") and "Cloned" not in name
+        return name.startswith("Spotify") and "Cloned" not in name and "AB" in name
+
+class Vendetta(modules.MergeSplitModule):
+    def __init__(self):
+        super.__init__("vendetta-arm64")
+
+    async def find_url(self):
+        async with AsyncClient() as client:
+            r = await client.get("https://discord.k6.tf/index.json")
+            versions = r.json()
+        version = versions['latest']['stable']
+        for name in ["base-360-lspatched", "split_config.arm64_v8a-360-lspatched","split_config.en-360-lspatched","split_config.xxhdpi-360-lspatched"]:
+            yield f"https://discord.k6.tf/{version}/{name}.apk"
 
 
 modules = [
     YoutubeRevancedExtended(),
-    XManager() # Spotify, Spotify Lite
+    Spotify(),
+    Vendetta()
 ]
--- downloader/modules/classes.py
+++ downloader/modules/classes.py
@@ -16,10 +16,9 @@ class DownloadModule():
 
 class MergeSplitModule(DownloadModule):
 
-    def __init__(self, folder, file):
-        self.folder = folder
-        self.file = file
-        os.mkdirs(folder, exists_ok=True)
+    def __init__(self, name):
+        self.name = name
+        os.mkdirs(name, exists_ok=True)
 
     async def find_url(self) -> Iterator[str]:
         raise NotImplementedError()
@@ -28,13 +27,13 @@ class MergeSplitModule(DownloadModule):
         async for url in self.find_url():
             name = url.split("/")[-1]
             print(f"Downloading {name} from {url}")
-            await asyncio.create_subprocess_exec("wget", "--show-progress", url, "-O", f"{self.folder}/{name}")
+            await asyncio.create_subprocess_exec("wget", "--show-progress", url, "-O", f"{self.name}/{name}")
 
     async def download(self):
         # join apks
         await self.download_splits()
         print("merging splits")
-        await asyncio.create_subprocess_exec("java", "-jar", "APKEditor.jar", "m", "-i", self.folder)
+        await asyncio.create_subprocess_exec("java", "-jar", "APKEditor.jar", "m", "-i", self.name)
         await asyncio.create_subprocess_exec("jarsigner", "-verbose","-sigalg","SHA1withRSA",
-            "-digestalg","SHA1","-keystore", "fdroid/keystore.jks",f"{self.folder}_merged.apk", "repokey", "-storepass", os.getenv("KEYSTOREPASS"))
-        await asyncio.create_subprocess_exec("zipalign", "-v", "4", f"{self.folder}_merged.apk", f"fdroid/repo/{self.file}")
+            "-digestalg","SHA1","-keystore", "fdroid/keystore.jks",f"{self.name}_merged.apk", "repokey", "-storepass", os.getenv("KEYSTOREPASS"))
+        await asyncio.create_subprocess_exec("zipalign", "-v", "4", f"{self.name}_merged.apk", f"fdroid/repo/{self.name}")
