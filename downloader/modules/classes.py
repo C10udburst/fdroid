3f3db5263f9d7c35fe52edcab87611fcd7783382
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2023-05-01 10:58:46 +0200

    Revert "Update classes.py"
    This reverts commit 3621e09b5d80b2638a9237004aeb113479e01442.

--- downloader/modules/classes.py
+++ downloader/modules/classes.py
@@ -37,3 +37,5 @@ class MergeSplitModule(DownloadModule):
         print("merging splits")
         process = await asyncio.create_subprocess_exec("java", "-jar", "APKEditor.jar", "m", "-i", self.name, "-o", f"fdroid/repo/{self.name}.apk")
         await process.wait()
+        process = await asyncio.create_subprocess_exec("apksigner", "sign", "-v", "--ks", "fdroid/keystore.jks", "--ks-key-alias", "repokey", "--ks-pass", "env:KEYSTOREPASS", "--out", f"fdroid/repo/{self.name}.apk", f"{self.name}_merged.apk")
+        await process.wait()
